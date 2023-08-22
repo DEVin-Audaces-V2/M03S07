325377fb6fdc0889db68bf9f3babfeca528e8ddd
tiagoamp <tiagoamp@gmail.com>
2023-08-21 21:41:54 -0300

    Primeiro conceitos de clean code

---
+++ clean-code/index.ts
@@ -0,0 +1,57 @@
+
+let nomeParaCracha = 'tiago';
+let nomeParaMatricula = 'tiago albuquerque';
+
+
+let tempoEmMinutos = 15;  
+
+
+let arr = [];
+for (let i = 0; i < arr.length; i++) {
+
+
+}
+
+// Usado paramentro somente com nome pq eh o suficiente para buscar o fubcionario,
+// para otimizar o algoritmo de bsuca 
+function recuperarFuncionarioAtivo(nome: string) {
+    console.log('faz algo');
+}
+
+try {
+    // faz alguma coisa 
+
+    // trecho gigante de código
+
+    //FIXME: refatorar esta funcao qubrando em funcoes menores 
+
+} catch (err) {
+    console.log(err);
+    //TODO: Implementar tratamento de erro adequado
+}
+
+
+const isConvidadoPermitido = idade > 18 && convite == 'valido' && nomeNaLista === true;
+if (isConvidadoPermitido) {
+    // pode entrar na festa
+}
+
+
+if (pessoa.temRepresentanteLegal()) {    
+     // usar dados do representante legal
+}
+
+
+
+class Pessoa {
+
+    repLegal: boolean ;
+
+    temRepresentanteLegal() {
+        return this.getCpfReLegal() !== null;
+    }
+
+}
+
+
+
